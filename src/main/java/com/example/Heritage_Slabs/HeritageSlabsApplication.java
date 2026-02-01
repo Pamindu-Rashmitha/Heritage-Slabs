77f2680b45c6dcf1aa5b595ed885da5a15952dcf
Pamindu-Rashmitha <pamidurashmitha@gmail.com>
2026-02-01 23:07:01 +0530

    Initial Spring Boot project with Maven wrapper
    Add initial scaffold for the Heritage-Slabs Spring Boot application. Includes Maven Wrapper and the needed dependencies.

---
+++ src/main/java/com/example/Heritage_Slabs/HeritageSlabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Heritage_Slabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritageSlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritageSlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Heritage_Slabs/HeritageSlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Heritage_Slabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageSlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
