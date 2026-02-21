44e3d57a7224be75101b8d18e320953dc76dd47c
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-02-21 07:46:47 +0530

    Update OrderRepository.java

--- backend/src/main/java/com/example/Heritage_Slabs/repository/OrderRepository.java
+++ backend/src/main/java/com/example/Heritage_Slabs/repository/OrderRepository.java
@@ -2,7 +2,10 @@ package com.example.Heritage_Slabs.repository;
 
 import com.example.Heritage_Slabs.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Long>
-{
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.user_id.email = ?1")
+    List<Order> findByUserEmail(String email);
 }
