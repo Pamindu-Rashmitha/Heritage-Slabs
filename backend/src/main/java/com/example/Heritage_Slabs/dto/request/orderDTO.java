f4c61e571afc93a764ef9759b0ff4f9ba46662f5
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-04-02 14:04:54 +0530

    Update orderDTO.java

--- backend/src/main/java/com/example/Heritage_Slabs/dto/request/orderDTO.java
+++ backend/src/main/java/com/example/Heritage_Slabs/dto/request/orderDTO.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,4 +38,20 @@ public class orderDTO {
     private String address;
 
     private List<orderItemDTO> items;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{5}$", message = "Postal code must be 5 digits")
+    private String postalCode;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String province;
+
+    @NotBlank
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone number")
+    private String phoneNumber;
+
+    private String Order_note;
 }
