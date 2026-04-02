ab4ed19680dc300e7bfb1b567cffff40effc2726
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-04-02 14:05:00 +0530

    Update Order.java

--- backend/src/main/java/com/example/Heritage_Slabs/model/Order.java
+++ backend/src/main/java/com/example/Heritage_Slabs/model/Order.java
@@ -25,6 +25,7 @@ public class Order {
     @Column(nullable = false)
     private double totalAmount;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
 
@@ -32,7 +33,21 @@ public class Order {
     private Date date;
 
     @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String province;
+
+    @Column(nullable = false)
     private String address;
 
+    @Column(nullable = false, length = 20)
+    private String PhoneNumber;
+
+    @Column(length = 500)
+    private String Order_note;
+
 }
-//hj
